99f3b90dfa795709e27ad3ca8566912780eae67b
deelaurence <laurie143pelumi@gmail.com>
2024-01-29 15:28:55 +0100

    python manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+print("Current Working Directory:", os.getcwd())
 
 def main():
     """Run administrative tasks."""
